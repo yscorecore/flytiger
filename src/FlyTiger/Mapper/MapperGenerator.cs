59037cd331215891999a8c435d90543f5f8dafb6
yangsi <obpt@sina.com>
2026-02-06 11:39:28 +0800

    migrate MapperGenerator to IIncrementalGenerator

--- src/FlyTiger/Mapper/MapperGenerator.cs
+++ src/FlyTiger/Mapper/MapperGenerator.cs
@@ -1,46 +1,65 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using FlyTiger.Mapper.Analyzer;
 using FlyTiger.Mapper.Generators;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace FlyTiger.Mapper
 {
     [Generator]
-    partial class MapperGenerator : ISourceGenerator
+    partial class MapperGenerator : IIncrementalGenerator
     {
         const string NameSpaceName = nameof(FlyTiger);
         public const string MapperExtensionName = "MapperExtensions";
 
-        public void Initialize(GeneratorInitializationContext context)
+
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            context.RegisterForPostInitialization((i) =>
+            // Post init - add attribute definitions
+            context.RegisterPostInitializationOutput((i) =>
             {
                 i.AddSource($"{AttributeFullName}.g.cs", AttributeCode);
                 i.AddSource($"{EFCoreQueryableExtensionsFullName}.g.cs", EFCoreExtensionsCode);
             });
-            context.RegisterForSyntaxNotifications(() => new MapperSyntaxReceiver());
-        }
 
-        public void Execute(GeneratorExecutionContext context)
-        {
-            if (!(context.SyntaxReceiver is MapperSyntaxReceiver receiver))
-                return;
-            var codeWriter = new CodeWriter(context);
+            // Syntax provider: find candidate class declarations
+            var classSymbols = context.SyntaxProvider
+                .CreateSyntaxProvider(
+                    predicate: (node, _) => node is ClassDeclarationSyntax cds && cds.AttributeLists.Any(),
+                    transform: (genCtx, ct) =>
+                    {
+                        var classDecl = (ClassDeclarationSyntax)genCtx.Node;
+                        return genCtx.SemanticModel.GetDeclaredSymbol(classDecl) as INamedTypeSymbol;
+                    })
+                .Where(s => s != null)
+                .Collect();
 
-            var attributes = FindAllAttributes(codeWriter, receiver.CandidateClasses);
-            if (attributes.Count > 0)
+            // Combine with compilation so we can inspect referenced assemblies
+            var compilationAndClasses = context.CompilationProvider.Combine(context.ParseOptionsProvider).Combine(classSymbols);
+
+
+            context.RegisterSourceOutput(compilationAndClasses, (spc, source) =>
             {
-                var codeFile = new CodeFile
+                var ((compilation, parseOptions), classes) = source;
+                if (classes.IsDefaultOrEmpty)
                 {
-                    BasicName = MapperExtensionName,
-                    Content = BuildMapperContent(codeWriter, attributes),
-                };
-                codeWriter.WriteCodeFile(codeFile);
-            }
+                    return;
+                }
+                var codeWriter = new CodeWriter(parseOptions, compilation, spc);
+                var attributes = FindAllAttributes(codeWriter, classes);
+                if (attributes.Count > 0)
+                {
+                    var codeFile = new CodeFile
+                    {
+                        BasicName = MapperExtensionName,
+                        Content = BuildMapperContent(codeWriter, attributes),
+                    };
+                    codeWriter.WriteCodeFile(codeFile);
+                }
+            });
         }
-
         private string BuildMapperContent(CodeWriter codeWriter, IList<AttributeData> attributeDatas)
         {
             CsharpCodeBuilder codeBuilder = new CsharpCodeBuilder("CS1591");
@@ -67,26 +86,11 @@ namespace FlyTiger.Mapper
             codeBuilder.EndAllSegments();
             return codeBuilder.ToString();
         }
-        private IList<AttributeData> FindAllAttributes(CodeWriter codeWriter, IList<ClassDeclarationSyntax> candidateClasses)
+        private IList<AttributeData> FindAllAttributes(CodeWriter codeWriter, IList<INamedTypeSymbol> classes)
         {
-            return codeWriter.GetAllClassSymbolsIgnoreRepeated(candidateClasses)
+            return classes
                   .SelectMany(p => p.GetAttributes().Where(t => t.AttributeClass.Is(AttributeFullName)))
                   .ToList();
-        }
-
-
-        private class MapperSyntaxReceiver : ISyntaxReceiver
-        {
-            public IList<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
-
-            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-            {
-                if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax &&
-                    classDeclarationSyntax.AttributeLists.Any())
-                {
-                    CandidateClasses.Add(classDeclarationSyntax);
-                }
-            }
         }
 
     }
